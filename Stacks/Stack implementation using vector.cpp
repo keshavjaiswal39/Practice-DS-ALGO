691920b7d4077d9e6cbd023736f8e7cdef91a7d2
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-09 10:33:33 +0530

    Reverse stack using one extra stack

---
+++ Stacks/Stack implementation using vector.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class stack{
+	private:
+		vector<int> v;
+		
+	public:
+		void push(int data)
+		{
+			v.push_back(data);
+		}
+		
+		bool empty()
+		{
+			return v.size() == 0;
+		}
+		
+		void pop()
+		{
+			v.pop_back();
+		}
+		
+		int top()
+		{
+			return v[v.size()-1];
+		}
+};
+
+
+int main()
+{
+	stack s;
+	
+	for(int i=0;i<20;i++)
+	{
+		s.push(i*i);
+	}
+	
+	while(!s.empty())
+	{
+		cout<<s.top()<<" ";
+		s.pop();
+	}
+	
+}
+
+
+
+
+
